5c4c1309cb641a75f75a1cbb3c62f8cbd5feba3a
Matthew Toohey <contact@mtoohey.com>
2023-10-30 20:35:23 -0400

    eof and pre-closebrace semicolon insertion

--- src/scanner.c
+++ src/scanner.c
@@ -1,26 +1,71 @@
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include <tree_sitter/parser.h>
 #include <wctype.h>
 
 enum TokenType { OpenBrace, CloseBrace, Semicolon, RawString };
 
-void *tree_sitter_koka_external_scanner_create() { return NULL; }
+struct scanner {
+  bool report_close_brace_after_semi_insert;
+  bool eof_semi_inserted;
+};
 
-void tree_sitter_koka_external_scanner_destroy(void *payload) {}
+void scanner_reset(struct scanner *scanner) {
+  scanner->report_close_brace_after_semi_insert = false;
+  scanner->eof_semi_inserted = false;
+}
+
+void *tree_sitter_koka_external_scanner_create() {
+  struct scanner *scanner = malloc(sizeof(struct scanner));
+  scanner_reset(scanner);
+  return scanner;
+}
+
+void tree_sitter_koka_external_scanner_destroy(void *payload) { free(payload); }
 
 unsigned tree_sitter_koka_external_scanner_serialize(void *payload,
                                                      char *buffer) {
-  return 0;
+  _Static_assert(sizeof(struct scanner) <=
+                     TREE_SITTER_SERIALIZATION_BUFFER_SIZE,
+                 "not enough room in buffer for scanner");
+  memcpy(buffer, payload, sizeof(struct scanner));
+  return sizeof(struct scanner);
 }
 
 void tree_sitter_koka_external_scanner_deserialize(void *payload,
                                                    const char *buffer,
-                                                   unsigned length) {}
+                                                   unsigned length) {
+  struct scanner *scanner = payload;
+  scanner_reset(scanner);
+
+  if (length == 0) {
+    return;
+  }
+
+  assert(length == sizeof(struct scanner) && "invalid length");
+  memcpy(payload, buffer, sizeof(struct scanner));
+}
 
 bool tree_sitter_koka_external_scanner_scan(void *payload, TSLexer *lexer,
                                             const bool *valid_symbols) {
+  struct scanner *scanner = payload;
+  if (scanner->report_close_brace_after_semi_insert) {
+    assert(lexer->lookahead == '}' && "unexpected lookahead");
+    scanner->report_close_brace_after_semi_insert = false;
+    lexer->result_symbol = CloseBrace;
+    lexer->advance(lexer, false);
+    return true;
+  }
+
   while (iswspace(lexer->lookahead))
     lexer->advance(lexer, true);
+
+  if (lexer->eof(lexer) && !scanner->eof_semi_inserted) {
+    scanner->eof_semi_inserted = true;
+    lexer->result_symbol = Semicolon;
+    return true;
+  }
 
   switch (lexer->lookahead) {
   case '{':
@@ -37,8 +82,8 @@ bool tree_sitter_koka_external_scanner_scan(void *payload, TSLexer *lexer,
       break;
     }
 
-    lexer->result_symbol = CloseBrace;
-    lexer->advance(lexer, false);
+    lexer->result_symbol = Semicolon;
+    scanner->report_close_brace_after_semi_insert = true;
     return true;
 
   case ';':
